16532a8ffc6d394c1cfe29fb277c393bcc81f1a3
Camilla Teodoro <camillateodoro@hotmail.com>
2024-04-18 20:56:32 -0300

    Modal conforme politica escolhida

--- WOSS/src/app/home/components/modal/modal.component.ts
+++ WOSS/src/app/home/components/modal/modal.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-modal',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./modal.component.scss']
 })
 export class ModalComponent {
+  constructor(
+    public dialogRef: MatDialogRef<ModalComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any) {}
 
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
 }
--- WOSS/src/app/home/components/opcoes/opcoes.component.ts
+++ WOSS/src/app/home/components/opcoes/opcoes.component.ts
@@ -22,17 +22,28 @@ export class OpcoesComponent {
     { image: 'assets/imagem_mock.png', title: 'SJF', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' }
   ];
 
+  modalContent: { [key: string]: string } = {
+    'Round Robin': 'É uma política de escalonamento de tempo compartilhado, onde cada processo recebe um pequeno intervalo de tempo (quantum) para ser executado na CPU. Quando o quantum acaba, o processo é colocado no final da fila e o próximo processo é executado. Essa política garante que todos os processos recebam um tempo justo de CPU e evita que um processo monopolize a CPU por muito tempo.',
+    'FIFO': 'First In, First Out é uma política de escalonamento simples, onde os processos são executados na ordem em que foram colocados na fila. O primeiro processo a chegar na fila é o primeiro a ser executado na CPU. Essa política pode levar a problemas de inanição (starvation), onde um processo de baixa prioridade pode ficar preso na fila enquanto outros processos são executados.',
+    'Prioridade': 'É uma política de escalonamento onde cada processo é atribuído uma prioridade. Processos com prioridades mais altas são executados antes dos processos com prioridades mais baixas. Essa política permite que processos críticos sejam executados rapidamente, mas pode levar a problemas de inanição se um processo de baixa prioridade nunca for executado',
+    'SJF': 'Shortest Job First é uma política de escalonamento onde o processo com o menor tempo de execução é executado primeiro. Essa política minimiza o tempo médio de espera dos processos na fila, mas pode levar a problemas de inanição se houver sempre processos mais curtos chegando na fila. Pode ser com ou sem preempção.'
+  };
+
   selectCard(index: number) {
     if (this.selectedCardIndex === null || this.selectedCardIndex === index) {
       this.selectedCardIndex = this.selectedCardIndex === index ? null : index;
     }
-    console.log("selecionado: " + index)
-  }
+  } 
 
   openModal(event: MouseEvent, index: number) {
     event.stopPropagation(); 
     this.selectCard(index);
-    this.dialog.open(ModalComponent);
+    const dialogRef = this.dialog.open(ModalComponent, {
+      data: {
+        title: this.cards[index].title,
+        content: this.modalContent[this.cards[index].title]
+      }
+    });
   }
 
   formatLabel(value: number): string {
